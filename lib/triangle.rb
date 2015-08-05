8c4fa3eb2d69d10f296bb0645e744daa611083ae
GabiOC <oconnor.gabriele@gmail.com>
2015-08-05 17:01:46 -0400

    all done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
   # write code here
+  # class TriangleError < StandardError
+  #   attr_reader :object
+
+  #   def initialize(triangl)
+  #     @triangle = triangle
+  #   end
+  # end
+
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+    raise TriangleError if x == 0 && y == 0 && z == 0
+    raise TriangleError if x < 0 || y < 0 || z < 0
+    raise TriangleError if ((x + y) <= z) || ((x + z) <= y) || ((y + z) <= x)
+  end
+
+  def kind
+    if @x == @y && @x == @z
+      :equilateral
+    elsif @x == @y || @x == @z || @y == @z
+      :isosceles
+    elsif @x != @y && @x != @z && @y != @z
+      :scalene
+    end
+  end
+end
+
+
+class TriangleError < Exception
+  puts "This is an illegal triangle"
 end
